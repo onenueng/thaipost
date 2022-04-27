d3e55379387433076e7d8f217152094a1b620172
onenueng <saipin.plu@mahidol.edu>
2022-04-27 13:14:26 +0700

    subdistrict table

--- database/migrations/2022_04_27_051809_create_subdistricts_table.php
+++ database/migrations/2022_04_27_051809_create_subdistricts_table.php
@@ -21,14 +21,14 @@ return new class extends Migration
             $table->timestamps();
         });
 
-        Subdistrict::create(['code' =>100402 , 'namethai' => 'แขวงสีลม','nameeng' =>'Khwaeng Si Lom']);
-        Subdistrict::create(['code' =>100403 , 'namethai' => 'แขวงสุริยวงศ์','nameeng' =>'Khwaeng Suriyawong']);
-        Subdistrict::create(['code' =>101802 , 'namethai' => 'แขวงคลองสาน','nameeng' =>'Khwaeng Khlong San']);
-        Subdistrict::create(['code' =>101803 , 'namethai' => 'แขวงบางลำภูล่าง','nameeng' =>'Khwaeng Bang Lamphu Lang']);
-        Subdistrict::create(['code' =>610501 , 'namethai' => 'หนองขาหย่าง','nameeng' =>'Nong Khayang']);
-        Subdistrict::create(['code' =>610502 , 'namethai' => 'หนองไผ่','nameeng' =>'Nong Phai']);
-        Subdistrict::create(['code' =>610601 , 'namethai' => 'บ้านไร่','nameeng' =>'Ban Rai']);
-        Subdistrict::create(['code' =>610602 , 'namethai' => 'ทัพหลวง','nameeng' =>'Thap Luang']);
+        Subdistrict::create(['code' =>100402, 'namethai' => 'แขวงสีลม','nameeng' =>'Khwaeng Si Lom']);
+        Subdistrict::create(['code' =>100403, 'namethai' => 'แขวงสุริยวงศ์','nameeng' =>'Khwaeng Suriyawong']);
+        Subdistrict::create(['code' =>101802, 'namethai' => 'แขวงคลองสาน','nameeng' =>'Khwaeng Khlong San']);
+        Subdistrict::create(['code' =>101803, 'namethai' => 'แขวงบางลำภูล่าง','nameeng' =>'Khwaeng Bang Lamphu Lang']);
+        Subdistrict::create(['code' =>610501, 'namethai' => 'หนองขาหย่าง','nameeng' =>'Nong Khayang']);
+        Subdistrict::create(['code' =>610502, 'namethai' => 'หนองไผ่','nameeng' =>'Nong Phai']);
+        Subdistrict::create(['code' =>610601, 'namethai' => 'บ้านไร่','nameeng' =>'Ban Rai']);
+        Subdistrict::create(['code' =>610602, 'namethai' => 'ทัพหลวง','nameeng' =>'Thap Luang']);
         Subdistrict::create(['code' =>310112, 'namethai' => 'พระครู','nameeng' =>'Phra Khru']);
         Subdistrict::create(['code' =>310119, 'namethai' => 'บัวทอง','nameeng' =>'Bua Thong']);
         Subdistrict::create(['code' =>311001, 'namethai' => 'ลำปลายมาศ','nameeng' =>'Lam Plai Mat']);
